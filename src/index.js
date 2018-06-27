0599ac86ce037ade86ffb7e42eaa284c434661aa
Cole9350 <Cole9350@gmail.com>
2018-06-27 02:16:38 -0400

    converted to BrowserRouter for gh pages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router";
+import { BrowserRouter } from 'react-router-dom'
 
 import indexRoutes from "routes/index.jsx";
 
@@ -9,13 +10,14 @@ import "assets/scss/material-kit-react.css?v=1.1.0";
 
 var hist = createBrowserHistory();
 
+
 ReactDOM.render(
-  <Router history={hist}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Switch>
       {indexRoutes.map((prop, key) => {
-        return <Route path={`${process.env.PUBLIC_URL}` + prop.path} key={key} component={prop.component} />;
+        return <Route path={prop.path} key={key} component={prop.component} />;
       })}
     </Switch>
-  </Router>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
