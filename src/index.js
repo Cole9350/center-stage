628fcd5d160cc381d51430032bc5f4c5cdc5d5af
Cole9350 <Cole9350@gmail.com>
2018-10-21 19:01:04 -0400

    route test

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router";
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
-import indexRoutes from "routes/index.jsx";
+import indexRoutes from 'routes/index.jsx';
 
-import "assets/scss/material-kit-react.css?v=1.1.0";
+import 'assets/scss/material-kit-react.css?v=1.1.0';
 
 var hist = createBrowserHistory();
 
-
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} key={key} component={prop.component} />;
-      })}
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<Switch>
+			{indexRoutes.map((prop, key) => {
+				return <Route path={`${process.env.PUBLIC_URL}` + prop.path} key={key} component={prop.component} />;
+			})}
+		</Switch>
+	</BrowserRouter>,
+	document.getElementById('root')
 );
