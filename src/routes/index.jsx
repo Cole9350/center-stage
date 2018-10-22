045f88e09fc1c6c9cf1c1eee91f18838d934ea5e
Cole9350 <Cole9350@gmail.com>
2018-10-21 20:30:47 -0400

    Remove extra

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,7 @@
-import Components from 'views/Components/Components.jsx';
+//import Components from 'views/Components/Components.jsx';
 import LandingPage from 'views/LandingPage/LandingPage.jsx';
-import ProfilePage from 'views/ProfilePage/ProfilePage.jsx';
-import LoginPage from 'views/LoginPage/LoginPage.jsx';
+//import ProfilePage from 'views/ProfilePage/ProfilePage.jsx';
+//import LoginPage from 'views/LoginPage/LoginPage.jsx';
 
 var indexRoutes = [{ path: '/', name: 'LandingPage', component: LandingPage }];
 
