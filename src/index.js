86d9cf7562c12dd1027557fbc05079b23e05dbb3
Cole9350 <Cole9350@gmail.com>
2018-10-28 19:19:42 -0400

    revert

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
 import { Router, Route, Switch } from 'react-router';
-import LandingPage from 'views/LandingPage/LandingPage.jsx';
-var hist = createBrowserHistory();
+import { BrowserRouter } from 'react-router-dom';
 
+import indexRoutes from 'routes/index.jsx';
+
+import 'assets/scss/material-kit-react.css?v=1.1.0';
+
+var hist = createBrowserHistory();
 ReactDOM.render(
-	<Router history={hist}>
-		<LandingPage />
-	</Router>,
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<Switch>
+			{indexRoutes.map((prop, key) => {
+				return <Route path={`${process.env.PUBLIC_URL}` + prop.path} key={key} component={prop.component} />;
+			})}
+		</Switch>
+	</BrowserRouter>,
 	document.getElementById('root')
 );
